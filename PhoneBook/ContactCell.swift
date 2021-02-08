06cb7d18d6ff3c9e2b6b611d4cbdbb34b80bda8b
Akopov Bogdan <Neformallp@gmail.com>
2021-02-08 14:32:47 +0200

    Add new project file for contact cell.

---
+++ PhoneBook/ContactCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactCell.swift
+//  PhoneBook
+//
+//  Created by EvilB on 08.02.2021.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PhoneBook/Users.swift
@@ -0,0 +1,16 @@
+//
+//  Users.swift
+//  PhoneBook
+//
+//  Created by EvilB on 01.02.2021.
+//
+
+import Foundation
+
+
+struct Users {
+    var name: String
+    var surname: String
+    var phoneNumber: Int
+    var city: String
+}
