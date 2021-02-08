5178a16ecd97f81e2384710581c5a4dae6aba15a
Akopov Bogdan <Neformallp@gmail.com>
2021-02-08 15:45:11 +0200

    Add delete and move cells.

--- PhoneBook/Controllers/TableViewController.swift
+++ PhoneBook/Controllers/TableViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    var usersData = [Users(name: "Ivan", surname: "Ivanov", phoneNumber: +1234, city: "Kyiv"),
+    var usersStorage = [Users(name: "Ivan", surname: "Ivanov", phoneNumber: +1234, city: "Kyiv"),
                  Users(name: "Sergey", surname: "", phoneNumber: 1234, city: "Kyiv"),
                  Users(name: "Bogdan", surname: "Akopov", phoneNumber: 1234, city: "Kyiv")]
 
@@ -35,63 +35,37 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return usersData.count
+        return usersStorage.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ContactCell
 
-        let model = usersData[indexPath.row]
+        let model = usersStorage[indexPath.row]
         
         cell.set(object: model)
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            usersStorage.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
+    
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedContact = usersStorage.remove(at: sourceIndexPath.row)
+        usersStorage.insert(movedContact, at: destinationIndexPath.row)
+        tableView.reloadData()
     }
-    */
-
 }
