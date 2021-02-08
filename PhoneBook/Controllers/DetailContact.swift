b2a65479e98fbb9238e162cd7e60eadefb5f9cd6
Akopov Bogdan <Neformallp@gmail.com>
2021-02-08 16:27:41 +0200

    Add editing screen and setup interface.

---
+++ PhoneBook/Controllers/DetailContact.swift
@@ -0,0 +1,16 @@
+//
+//  DateilTableViewController.swift
+//  PhoneBook
+//
+//  Created by EvilB on 01.02.2021.
+//
+
+import UIKit
+
+class DetailContact: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
